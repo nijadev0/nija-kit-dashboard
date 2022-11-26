45b3e744fd88a23bc5f38b4dd34949ea0dfa4fbb
Afochar <aflowchars@gmail.com>
2022-11-26 22:14:38 +0700

    refactor code

--- src/components/pages/Customers.vue
+++ src/components/pages/Customers.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import BaseLayoutVue from '../templates/BaseLayout.vue';
+
 import Heading from '../atoms/Heading.vue';
 </script>
 
 <template>
-    <Heading size="lg" variant="bold">
-        Customers
-    </Heading>
+    <BaseLayoutVue title="Customers">
+        <Heading>
+            Ini Adalah Customers
+        </Heading>
+    </BaseLayoutVue>
 </template>

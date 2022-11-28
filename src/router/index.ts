7b65281e1e292046eda17ff0abe59f1ac71727e8
Afochar <aflowchars@gmail.com>
2022-11-28 17:35:12 +0700

    fixed some error

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,8 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '$components/pages/Home.vue';
 import Customers from '$components/pages/Customers.vue';
 import Products from '$components/pages/Products.vue';
-// import Sandbox from '$components/pages/Sandbox.vue';
-import NotFound from '$components/pages/404.vue';
+import Sandbox from '$components/pages/Sandbox.vue';
+import NotFound from '$components/pages/NotFound.vued.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -24,12 +24,16 @@ const router = createRouter({
             name: 'Products',
             component: Products
         },
-        // {
-        //     path: '/sandbox',
-        //     name: 'Sandbox',
-        //     component: Sandbox
-        // },
-        { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
+        {
+            path: '/sandbox',
+            name: 'Sandbox',
+            component: Sandbox
+        },
+        { 
+            path: '/:pathMatch(.*)*', 
+            name: 'NotFound', 
+            component: NotFound 
+        },
     ]
 })
 

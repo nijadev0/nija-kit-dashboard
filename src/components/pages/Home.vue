d89878b7ee6139ae55196081f3937372838e7dd3
Afochar <aflowchars@gmail.com>
2022-12-05 15:09:53 +0700

    fixed variant on Home and Not Found

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,32 +1,47 @@
 <script setup lang="ts">
-import BaseLayout from '$components/templates/BaseLayout.vue';
+import BaseLayout from '$components/templates/BaseLayout.vue'
 
-import Heading from '$components/atoms/Heading.vue';
-import Para from '$components/atoms/Para.vue';
-import Button from '$components/atoms/Button.vue';
+import Heading from '$components/atoms/Heading.vue'
+import Para from '$components/atoms/Para.vue'
+import Button from '$components/atoms/Button.vue'
 
-import WelcomeIll from '$assets/images/illustration-1.webp';
+import WelcomeIll from '$assets/images/illustration-1.webp'
 </script>
 
 <template>
-    <BaseLayout title="Nija Dashboard UI Kit">
-        <div class="min-h-screen h-full flex flex-col justify-center items-center">
-            <div class="-mt-56 flex flex-col justify-center items-center h-full">
-                <Heading size="md" variant="semibold" class="mb-8">Hello, Nija Visitors!</Heading>
-    
-                <img class="w-[28rem] rounded-[10px] mb-8" :src="WelcomeIll" alt="Welcome Visitor" />
-    
-                <Para size="xl" variant="base" class="text-netral-60 text-center max-w-3xl mb-8">
-                    We are currently working on this template. This template contains several base style for our internal Dashboard UI kit.
-                    If you found any error, unmatching styles, or any other that feels like not okay, dont worry to send feedback in our Discord, Thank You.
-                </Para>
-    
-                <Button type="background" variant="primary" size="lg">
-                    <a target="_blank" href="https://discord.com/channels/907632044375425065/907632044840980486">
-                        Send your message
-                    </a>
-                </Button>
-            </div>
-        </div>
-    </BaseLayout>
+  <BaseLayout title="Nija Dashboard UI Kit">
+    <div class="flex h-full min-h-screen flex-col items-center justify-center">
+      <div class="-mt-56 flex h-full flex-col items-center justify-center">
+        <Heading size="md" variant="semibold" class="mb-8"
+          >Hello, Nija Visitors!</Heading
+        >
+
+        <img
+          class="mb-8 w-[28rem] rounded-[10px]"
+          :src="WelcomeIll"
+          alt="Welcome Visitor"
+        />
+
+        <Para
+          size="xl"
+          variant="medium"
+          class="mb-8 max-w-3xl text-center text-netral-60"
+        >
+          We are currently working on this template. This template contains
+          several base style for our internal Dashboard UI kit. If you found any
+          error, unmatching styles, or any other that feels like not okay, dont
+          worry to send feedback in our Discord, Thank You.
+        </Para>
+
+        <Button type="background" variant="primary" size="lg">
+          <a
+            target="_blank"
+            href="https://discord.com/channels/907632044375425065/907632044840980486"
+          >
+            Send your message
+          </a>
+        </Button>
+      </div>
+    </div>
+  </BaseLayout>
 </template>
--- src/components/pages/NotFound.vue
+++ src/components/pages/NotFound.vue
@@ -1,29 +1,41 @@
 <script setup lang="ts">
-import BaseLayout from '$components/templates/BaseLayout.vue';
+import BaseLayout from '$components/templates/BaseLayout.vue'
 
-import Heading from '$components/atoms/Heading.vue';
-import Para from '$components/atoms/Para.vue';
-import Button from '$components/atoms/Button.vue';
+import Heading from '$components/atoms/Heading.vue'
+import Para from '$components/atoms/Para.vue'
+import Button from '$components/atoms/Button.vue'
 
-import NotFoundIll from '$assets/images/illustration-2.webp';
+import NotFoundIll from '$assets/images/illustration-2.webp'
 </script>
 
 <template>
-    <BaseLayout title="Nija Dashboard UI Kit">
-        <div class="min-h-screen h-full flex flex-col justify-center items-center">
-            <div class="-mt-56 flex flex-col justify-center items-center h-full">
-                <Heading size="md" variant="semibold" class="mb-8">Hello, Nija Visitors!</Heading>
-    
-                <img class="w-[28rem] rounded-[10px] mb-8" :src="NotFoundIll" alt="Not Found Visitor" />
-    
-                <Para size="xl" variant="base" class="text-netral-60 text-center max-w-3xl mb-8">
-                    I know it's not like what you hope at all &mdash; because this dashboard is on progress, so for this page, please wait for several weeks later ya
-                </Para>
-    
-                <Button href="/" type="background" variant="primary" size="lg">
-                    Back to home
-                </Button>
-            </div>
-        </div>
-    </BaseLayout>
+  <BaseLayout title="Nija Dashboard UI Kit">
+    <div class="flex h-full min-h-screen flex-col items-center justify-center">
+      <div class="-mt-56 flex h-full flex-col items-center justify-center">
+        <Heading size="md" variant="semibold" class="mb-8"
+          >Hello, Nija Visitors!</Heading
+        >
+
+        <img
+          class="mb-8 w-[28rem] rounded-[10px]"
+          :src="NotFoundIll"
+          alt="Not Found Visitor"
+        />
+
+        <Para
+          size="xl"
+          variant="medium"
+          class="mb-8 max-w-3xl text-center text-netral-60"
+        >
+          I know it's not like what you hope at all &mdash; because this
+          dashboard is on progress, so for this page, please wait for several
+          weeks later ya
+        </Para>
+
+        <Button href="/" type="background" variant="primary" size="lg">
+          Back to home
+        </Button>
+      </div>
+    </div>
+  </BaseLayout>
 </template>
